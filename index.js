223cc0a92b91ccfec77314e975fac5f4c9d1fdd5
Ronald Barasa <barasaogoro@gmail.com>
2023-03-12 23:35:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let body = document.querySelector("body");
+let main = document.querySelector("main#main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "John is the champion";
+body.append(newHeader);
